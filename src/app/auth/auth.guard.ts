ff963434dd722fd625bec8a7a70756aa2f9271bf
Karolina Meira <karolina.meira@hotmail.com>
2023-09-04 16:15:45 -0300

    feat: integracao cadastro usuario

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -7,7 +7,6 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
   if(authService.isAuthenticated()){   
-    console.log('auth true');
     return true;
     
   } 
--- src/app/usuarios/cadastrar-usuarios/cadastrar-usuarios.component.ts
+++ src/app/usuarios/cadastrar-usuarios/cadastrar-usuarios.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuariosService } from '../usuarios.service';
 
 @Component({
   selector: 'app-cadastrar-usuarios',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class CadastrarUsuariosComponent {
 
+  public formUsuario: FormGroup = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    cpf: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    celular: new FormControl('', [Validators.required,]),
+    senha: new FormControl('', [Validators.required,]),
+  })
+
+  constructor(private usuariosService: UsuariosService) {}
+
+  public cadastrar() {
+    if(this.formUsuario.valid){
+      this.usuariosService.cadastrar(this.formUsuario.value).subscribe((res) => {
+        alert(res.msg)
+        this.formUsuario.reset();
+      }) 
+    } else {
+       alert('Verifique se todos os campos estão preenchidos')
+    }
+    
+  }
 }
