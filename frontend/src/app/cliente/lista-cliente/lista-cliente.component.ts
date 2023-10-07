8970b30e201916f2cfaebabf609e68104fe7c729
Karolina Meira <karolina.meira@hotmail.com>
2023-10-07 12:00:21 -0300

    feat: filtro lista cliente

--- frontend/src/app/cliente/lista-cliente/lista-cliente.component.ts
+++ frontend/src/app/cliente/lista-cliente/lista-cliente.component.ts
@@ -1,26 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../cliente.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-cliente',
   templateUrl: './lista-cliente.component.html',
   styleUrls: ['./lista-cliente.component.scss']
 })
-export class ListaClienteComponent implements OnInit{
+export class ListaClienteComponent implements OnInit {
 
-public clienteLoaded: boolean = false
-public clientes:any = []
+  public clienteLoaded: boolean = false
+  public clientes: any = []
+  public formPesquisarCliente: FormGroup = new FormGroup({
+    filtro: new FormControl('', Validators.required)
+  })
 
-constructor(private clienteService: ClienteService){}
+  constructor(private clienteService: ClienteService) { }
   ngOnInit(): void {
-    this.getAllClientes()
+    this.getAllClientes();
+
+    this.formPesquisarCliente.get('filtro')?.valueChanges.subscribe((value) => {
+      if (value == '') {
+        this.getAllClientes();
+      } else {
+        this.pesquisarCliente();
+      }
+    })
   }
 
-public getAllClientes(){
-  this.clienteService.getAllClientes().subscribe(res =>{
-    this.clientes = res
-    this.clienteLoaded = true
-  })
-}
+  public getAllClientes() {
+    this.clienteService.getAllClientes().subscribe(res => {
+      this.clientes = res
+      this.clienteLoaded = true
+    })
+  }
+
+  public pesquisarCliente() {
+    let data = this.formPesquisarCliente.get('filtro')?.value
+
+    if (data != '' && data != undefined) {
+      this.clienteService.getAllClientes().subscribe((clientes) => {
+        this.clientes = clientes.filter((cliente: any) => {
+          return cliente.cliente.match(data) || cliente.cpf.match(data) || cliente.endereco.match(data) || cliente.email.match(data) || cliente.celular.match(data)
+        })
+      })
+    }
+  }
 
 }
