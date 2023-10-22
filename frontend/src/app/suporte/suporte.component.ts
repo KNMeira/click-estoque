55a11634997272c1170e9bb989f113f9d2e835b1
Karolina Meira <karolina.meira@hotmail.com>
2023-10-22 13:03:27 -0300

    feat: links suporte

--- frontend/src/app/suporte/suporte.component.ts
+++ frontend/src/app/suporte/suporte.component.ts
@@ -6,11 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./suporte.component.scss']
 })
 export class SuporteComponent {
+  public showEmailCopiado: boolean = false;
+  public showTelCopiado: boolean = false;
 
   public setDarkMode(){
     let isDarkMode = localStorage.getItem('isDarkMode');
 
     return isDarkMode == 'true' ? 'dark' : '';
   };
+
+  public copyEmail() {
+    navigator.clipboard.writeText('suporte@exitoconsultoria.br');
+    this.showEmailCopiado = true;
+    setTimeout(() => {
+      this.showEmailCopiado = false;
+    }, 2000);
+  }
+
+  public copyTel() {
+    navigator.clipboard.writeText('(19) 97777-7777');
+    this.showTelCopiado = true;
+    setTimeout(() => {
+      this.showTelCopiado = false;
+    }, 2000);
+  }
 
 }
