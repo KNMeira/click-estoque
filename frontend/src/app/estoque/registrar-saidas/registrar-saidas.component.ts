cb336d34d2ef0e4b0746de5ab738e55941ed76e0
Karolina Meira <karolina.meira@hotmail.com>
2023-10-23 14:15:06 -0300

    bugfix layout e validacao

--- frontend/src/app/estoque/registrar-saidas/registrar-saidas.component.ts
+++ frontend/src/app/estoque/registrar-saidas/registrar-saidas.component.ts
@@ -23,21 +23,47 @@ export class RegistrarSaidasComponent {
       prods.forEach((peca:any) => {
         this.formRegistrarSaidas.addControl(peca.id_peca, new FormControl(0))
       });
-  
+      
       this.pecas = prods
       this.isPecasLoaded = true;
     })
   }
 
-  public registrarSaidas(){
+  public registrarSaidas(){    
+    if(this.isQntAttNegativa()) {
+      alert('Não é possível registrar saída de quantidade maior do que a quantidade em estoque. Verifique as quantidades e tente novamente')
+      return;
+    }
+
     this.isLoadingRegistro = true;
     const data = this.formRegistrarSaidas.value;
     this.estoqueService.registrarSaidas(data).subscribe((res) => {
       this.isLoadingRegistro = false;
       alert(res.msg)
       
-    })
-    
+    })    
   }  
 
+  public isQntAttNegativa(): boolean{
+    let qntAttArray: any = [];
+    Object.entries(this.formRegistrarSaidas.value).forEach((p:any) => {
+      const peca = this.pecas.find((e:any) => { return e.id_peca == p[0]})
+      const pecaQnt = peca.quantidade
+      const qntAtt = pecaQnt - p[1]
+      qntAttArray.push(qntAtt);
+    });
+    
+    let isQntAttNegativaVar: boolean = false;
+    qntAttArray.some((qnt:any) => {
+      
+      if( qnt < 0) {
+        isQntAttNegativaVar = true;
+        return true;
+      } else {
+        return false;
+      }      
+    })
+    
+    return isQntAttNegativaVar;
+  }
 }
